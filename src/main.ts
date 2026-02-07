a391203c029c408061a340c2eb4cf968de28da7e
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-02-08 00:39:52 +0200

    feat: migrate router configuration to TypeScript and update import paths

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
-import { router } from "./router/index";
+import { router } from "./router/index.js";
 
 const app = createApp(App);
 
